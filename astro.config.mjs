e93df1676bb4895135d45372c7a264a369e7c475
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-07-14 14:09:55 +0530

    remove git deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,11 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), icon()],
-	output: "static",
+	output: "server",
 	adapter: node({
 		mode: "standalone",
 	}),
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 	},
-	site: "https://shonebinu.github.io",
 });
