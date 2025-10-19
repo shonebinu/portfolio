f9d9fce54bde38232318469a6d73c2b8379255b2
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-10-19 22:33:02 +0530

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import { SITE_URL } from "./src/consts.ts";
+
+// https://astro.build/config
+export default defineConfig({
+  site: SITE_URL,
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      theme: "dark-plus",
+    },
+  },
+});
