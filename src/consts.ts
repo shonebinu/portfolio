7bcc58b6a0dfdbcd5d7cf89c21bd1bcf463f3a73
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-10-20 09:48:45 +0530

    add site url

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://example.com";
-export const SITE_TITLE = "Astro Blog";
+export const SITE_URL = "https://shonebinu.com";
+export const SITE_TITLE = "Shone's Portfolio";
 export const SITE_DESCRIPTION = "Welcome to my website!";
