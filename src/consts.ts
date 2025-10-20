d86fa06a378090309f79a193a3a9e37a251f4319
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-10-20 22:33:57 +0530

    change font to inter

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,36 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import SampleImage from "./assets/blog-placeholder-1.jpg";
 
 export const SITE_URL = "https://shonebinu.com";
 export const SITE_TITLE = "Shone's Portfolio";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const about = {};
+
+export const works = [
+  {
+    image: SampleImage,
+    title: "MGU Scraper",
+    tags: ["Streamlit", "Python", "BeautifulSoup"],
+    description:
+      "MGUScraper is a tool that simplifies the bulk extraction of student's exam results under MG University, Kottayam.",
+    sourceLink: "https://github.com/shonebinu/MGUScraper",
+    liveLink: "",
+  },
+  {
+    image: SampleImage,
+    title: "MGU Scraper",
+    tags: ["Streamlit", "Python", "BeautifulSoup"],
+    description:
+      "MGUScraper is a tool that simplifies the bulk extraction of student's exam results under MG University, Kottayam.",
+    sourceLink: "https://github.com/shonebinu/MGUScraper",
+    liveLink: "",
+  },
+];
+
+export const blog = {};
+
+export const header = {};
+
+export const footer = {};
