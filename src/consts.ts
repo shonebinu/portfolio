1379018c5179d9700e259d7ac63218b3b9c9d474
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-10-21 11:05:26 +0530

    change works to projects

--- src/consts.ts
+++ src/consts.ts
@@ -6,13 +6,22 @@ export const SITE_URL = "https://shonebinu.com";
 export const SITE_TITLE = "Shone's Portfolio";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 
-export const about = {};
+export const about = {
+  name: "Shone Binu",
+  tagLine: "Software Engineer / Full Stack Developer",
+  profilePic: "",
+  description:
+    " I’m a recent Sofware Engineer graduate who likes to keep tackling new problems. Currently I’m working on React and Node.js side of things. Outside of this, I work myself to improve myself on whatever I see fit.",
+  links: [{ logo: "", link: "" }],
+  resumeLink: "",
+  workExperience: [],
+};
 
-export const works = [
+export const projects = [
   {
     image: SampleImage,
     title: "MGU Scraper",
-    tags: ["Streamlit", "Python", "BeautifulSoup"],
+    techs: ["Streamlit", "Python", "BeautifulSoup"],
     description:
       "MGUScraper is a tool that simplifies the bulk extraction of student's exam results under MG University, Kottayam.",
     sourceLink: "https://github.com/shonebinu/MGUScraper",
@@ -21,7 +30,7 @@ export const works = [
   {
     image: SampleImage,
     title: "MGU Scraper",
-    tags: ["Streamlit", "Python", "BeautifulSoup"],
+    techs: ["Streamlit", "Python", "BeautifulSoup"],
     description:
       "MGUScraper is a tool that simplifies the bulk extraction of student's exam results under MG University, Kottayam.",
     sourceLink: "https://github.com/shonebinu/MGUScraper",
